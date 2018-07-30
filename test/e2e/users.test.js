76c260d1be52f9c2b989a507e4e91ec33649dd92
carriecarter <carrie@carrie-carter.com>
2018-07-30 15:35:13 -0700

    gear unit test model validation passing

--- test/e2e/users.test.js
+++ test/e2e/users.test.js
@@ -1,6 +1,6 @@
 const { assert } = require('chai');
 const request = require('./request');
-const { dropCollection  } = require('./_db');
+const { dropCollection } = require('./_db');
 
 const checkOk = res => {
     assert.equal(res.status, 200, 'expected 200 http status code');
