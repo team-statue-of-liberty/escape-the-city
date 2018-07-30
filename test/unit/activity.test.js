9e31705461c88b73904745b23fabe2c85b465c81
MariahAdams <mariah.adams23@yahoo.com>
2018-07-30 15:43:30 -0700

    Added activities files, starting unit tests

---
+++ test/unit/activity.test.js
@@ -0,0 +1,12 @@
+const chai = require('chai');
+const { assert } = chai;
+const { Types } = require('mongoose');
+const Activity = require('../../lib/models/activity');
+const { getErrors } = require('./helpers');
+
+describe.only('Activity model', () => {
+
+    it('validates a good activity model', () => {
+
+    });
+});
