a9337a6448efa9bf1cf7406d34cc27d7c05ac363
MariahAdams <mariah.adams23@yahoo.com>
2018-07-31 14:54:58 -0700

    setting up scaffolding for mock data

---
+++ test/e2e/_data.test.js
@@ -0,0 +1,9 @@
+const { execSync } = require('child_process');
+const { join } = require('path');
+const mongoose = require('mongoose');
+const usersDataFile = join(__dirname, )
+
+describe('mock data', () => {
+
+    
+});
