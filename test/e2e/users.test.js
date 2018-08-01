0688a5181f4149c5bbe2aad867291fcbc6efbb6c
carriecarter <carrie@carrie-carter.com>
2018-08-01 11:39:14 -0700

    users e2e signup passing

---
+++ test/e2e/users.test.js
@@ -0,0 +1,35 @@
+const { assert } = require('chai');
+const request = require('./request');
+const { dropCollection } = require('./_db');
+
+const checkOk = res => {
+    assert.equal(res.status, 200, 'expected 200 http status code');
+    return res;
+};
+
+describe('Users API', () => {
+    
+    beforeEach(() => dropCollection('users'));
+
+    let token;
+    beforeEach(() => {
+        return request  
+            .post('/api/auth/signup')
+            .send({
+                email: 'Chris@test.com',
+                firstName: 'Chris',
+                driver: false,
+                password: 'pass123'
+            })
+            .then(checkOk)
+            .then(({ body }) => {
+                token = body.token;
+            });
+    });
+
+    it('signs up the user', () => {
+        assert.isDefined(token);
+    });
+
+});
+
