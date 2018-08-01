e78995328af6a1e9479d43e22d343ed9d941eb7e
MariahAdams <mariah.adams23@yahoo.com>
2018-08-01 11:30:32 -0700

    augmented user data, gears now have appropriate owner ids

--- test/e2e/_data.test.js
+++ test/e2e/_data.test.js
@@ -23,7 +23,8 @@ describe.skip('seed data', () => {
     })
 
     // TERMINAL commands to take seed data and export back to json data files (to keep same ObjectId)
-    // mongoexport --db team_froggy_test --collection users --out users-data.json
-    // mongoexport --db team_froggy_test --collection bars --out bars-data.json
-    // mongoexport --db team_froggy_test --collection sales --out sales-data.json
+    // mongoexport --db escape --collection users --out users-data.json
+    // mongoexport --db escape --collection gears --out gears-data.json
+    // mongoexport --db escape --collection activities --out activities-data.json
+    // mongoexport --db escape --collection events --out events-data.json
 });
